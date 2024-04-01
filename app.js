289b7e847abecd35c5cdf0167f30a9a4cc43f374
Mridul1729 <129084013+Mridul1729@users.noreply.github.com>
2024-04-01 09:30:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let t = new Date("January 22, 2024 00:00:00").getTime();
+let t = new Date("April 16, 2024 00:00:00").getTime();
 let x = setInterval(function () {
     let now = new Date().getTime();
     let left = t - now ;
