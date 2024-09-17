bdf14e08cc0f0421289f093b1777ef2a65f06957
Mridul1729 <129084013+Mridul1729@users.noreply.github.com>
2024-09-17 22:10:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
 var countDownDate = new Date("Oct 24, 2024 00:00:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.queryselector(".d").innerHTML = days ;
-  document.queryselector(".h").innerHTML = hours ;
-  document.queryselector(".m").innerHTML = minutes ;
-  document.queryselector(".s").innerHTML = seconds ;
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.queryselector(".timer").style.display = "none";
-    document.queryselector(".text").innerHTML = "Happy Birthday My Cute & ______ Friend 😁😁"
-  }
-}, 1000);
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("count").style.display = "none";
+        document.getElementById("text").innerText = "Happy Birthday My Cute & ______ Friend 😁😁";
+    }
+
+    document.getElementById('day').innerText = days ;
+    document.getElementById('hour').innerText = hours ;
+    document.getElementById('minute').innerText = minutes ;
+    document.getElementById('second').innerText = seconds ;
+
+})
